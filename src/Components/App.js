3d95750ad0ddc1c4016934f16eb38e19e1363e51
in-kim <ehddls960617@gmail.com>
2021-01-18 18:55:32 +0900

    global style 지정

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import GlobalStyles from "Components/GlobalStyles";
 
 class App extends Component{
   render(){
-    return <div className="App"></div>
+    return (
+      <>
+        <GlobalStyles />
+        <div className="App">안녕하세요. 폰트 테스트 입니다.</div>
+      </>
+    )
   }
 }
 
--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -3,6 +3,20 @@ import reset from "styled-reset";
 
 const globalStyles = createGlobalStyle`
   ${reset};
+  @font-face {
+    font-family: 'SDKukdetopokki';
+    src: url('/assets/fonts/SDKukdetopokki-aLt.woff') format('woff');
+    font-style: normal;
+    font-weight: 400;
+    font-display: fallback;
+  }
+  @font-face {
+    font-family: 'SDKukdetopokki';
+    src: url('/assets/fonts/SDKukdetopokki-bBd.woff') format('woff');
+    font-style: normal;
+    font-weight: 700;
+    font-display: fallback;
+  }
   a{
     text-decoration:none;
     color:inherit;
@@ -11,8 +25,9 @@ const globalStyles = createGlobalStyle`
     box-sizing:border-box;
   }
   body{
-    font-family:--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size:12px;
+    font-family:'SDKukdetopokki';
+    font-size:14px;
+    font-weight:400;
     background-color:rgba(20, 20, 20, 1);
     color:#fff;
     padding-top:50px;
