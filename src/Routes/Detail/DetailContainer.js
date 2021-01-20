6ae21ed1f7e5f3b46c4c63bc1bb65cb5ad635ce2
in-kim <ehddls960617@gmail.com>
2021-01-20 18:37:41 +0900

    파라미터 값 저장 및 api 요청

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import DetailPrecenter from 'Routes/Detail/DetailPresenter';
+import {matchList} from 'api';
 // import api from api;
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default class extends React.Component {
   state= {
-    matchList:null,
+    result:null,
     error:null,
     loading:true
   }
 
   async componentDidMount(){
     try{
-
+      const {match: {params:{id}}, match: {params:{name}}} = this.props;
+      
+      console.log(await matchList.detail(id))
     }catch{
 
     }finally{
@@ -21,8 +24,13 @@ export default class extends React.Component {
   }
 
   render(){
+    const { result, error, loading } = this.state;
     return(
-      <DetailPrecenter />
+      <DetailPrecenter 
+        result={result}
+        error={error}
+        loading={loading}
+      />
     )
   }
 }
