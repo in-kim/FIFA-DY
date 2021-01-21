d560135635d8acac130f57944bd5a8f16dfebc0e
in-kim <ehddls960617@gmail.com>
2021-01-21 18:31:24 +0900

    로딩 컴포넌트 추가

---
+++ src/Components/Loding.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Item = styled.span`
+  font-size:90px;
+`;
+
+const Loading = () => (
+  <Item>로딩중입니다.</Item>
+)
+
+export default Loading;
