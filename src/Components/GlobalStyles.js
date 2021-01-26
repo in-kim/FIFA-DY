aad34fb36abfa62c86407ea78acc5b3de270eaa9
in-kim <ehddls960617@gmail.com>
2021-01-26 17:38:54 +0900

    bazzi 폰트로 변경

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -17,6 +17,13 @@ const globalStyles = createGlobalStyle`
     font-weight: 700;
     font-display: fallback;
   }
+  @font-face {
+    font-family: 'Bazzi';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/Bazzi.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   a{
     text-decoration:none;
     color:inherit;
@@ -25,7 +32,8 @@ const globalStyles = createGlobalStyle`
     box-sizing:border-box;
   }
   body{
-    font-family:'SDKukdetopokki';
+    /* font-family:'SDKukdetopokki'; */
+    font-family:'Bazzi';
     font-size:14px;
     font-weight:400;
     background-color:#222;
