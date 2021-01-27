b750f653c689472b0d8c5e4dd301323ed4d3d481
in-kim <ehddls960617@gmail.com>
2021-01-27 13:21:04 +0900

    선수 라인업리스트 추가

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -2,397 +2,105 @@ import React from "react";
 import PropTypes from 'prop-types';
 import styled from "styled-components";
 import Loading from 'Components/Loding';
-import Player from 'Components/Player';
+import Stadium from 'Components/Stadium';
 
-const Container = styled.div`
-  width:100%;
-  height:100%;
-  padding:50px;
-`;
-const Cover = styled.div`
+const PlayerList = styled.div`
   position:absolute;
-  left:0;
-  top:0;
-  width:100%;
-  height:100%;
-  background:url(${props => props.bgImage}) no-repeat center;
-  opacity:0.3;
-  z-index:0;
-`;
-const VersusContainer = styled.div`
-  position:absolute;
-  top:5%;
-  left:50%;
-  transform:translateX(-50%);
+  top: calc(5% + 130px);
+  ${props => props.wide};
+  z-index:1;
   display:flex;
-  align-items:center;
-  width:80%;
-  height:100px;
-  text-align:center;
+  flex-direction:column;
+  background:rgba(0,0,0,0.3);
+  color:#fff;
+  font-size:15px;
+  padding:10px;
 `;
 
-const Nickname = styled.span`
-  flex:1; 
-  font-size:50px;
-  text-shadow: 4px 3px 0 #7A7A7A;
+const Title = styled.span`
+  font-size:1.3rem;
+  margin-bottom:20px;
 `;
 
-const Price = styled.span`
-  display:block;
-  text-align:center;
-  font-size:25px;
-  margin-top:20px;
-  text-shadow:none;
-`;
-const Versus = styled.span`flex:0 0 100px;`;
-const VersusFirst = styled.span`
-  font-size:70px;
-  text-shadow: #FFF 0px 0px 5px, #FFF 0px 0px 10px, #FFF 0px 0px 15px, #3498db 0px 0px 20px, #3498db 0px 0px 30px, #3498db 0px 0px 40px, #3498db 0px 0px 50px, #3498db 0px 0px 75px;
-`;
-const VersusLast = styled.span`
-  font-size:50px;
-  text-shadow: #FFF 0px 0px 5px, #FFF 0px 0px 10px, #FFF 0px 0px 15px, #e74c3c 0px 0px 20px, #e74c3c 0px 0px 30px, #e74c3c 0px 0px 40px, #e74c3c 0px 0px 50px, #e74c3c 0px 0px 75px;
-`;
-
-const SoccerField = styled.div`
-  position:absolute;
-  top:calc(5% + 130px);
-  left:50%;
-  transform:translateX(-50%);
+const PlayerListItem = styled.span`
   display:flex;
-  width:69%;
-  height:830px;
-  background:url(${props => props.bgImage}) no-repeat;
-  background-size:contain;
+  margin-bottom:10px;
+  justify-content:space-between;
+  order: ${props=>props.order};
 `;
 
-const TeamContainer = styled.div`
-  display:flex;
-  flex:1;
-  max-width:50%;
-  position:relative;
+const Name = styled.span`
+  margin-right:10px;
 `;
-
-const PositionArea = styled.div`
-  position:relative;
-  flex:0 0 14.28%;
-  max-width:14.28%;
+const Position = styled.span`
+  background-color:${props => props.bgColor};
+  margin-right:5px;
+  padding:0 2px;
 `;
+const Price = styled.span``;
 
 const DetailPrecenter = ({PlayerResult, ClubPrice, error, loading}) => (
   <>
-    {
-      loading ? <Loading /> :
-      <Container>
-        <Cover bgImage={`${process.env.REACT_APP_URL}/assets/image/bg_stadium.jpg`}></Cover>
-        <VersusContainer>
-          <Nickname>
-            {PlayerResult.myNickName}
-            <Price>
-              {ClubPrice.myClub.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} BP
-            </Price>
-          </Nickname>
-          <Versus>
-            <VersusFirst>V</VersusFirst>
-            <VersusLast>S</VersusLast>
-          </Versus>
-          <Nickname>
-            {PlayerResult.enemyNickName}
-            <Price>
-              {ClubPrice.enemyClub.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} BP
-            </Price>
-          </Nickname>
-        </VersusContainer>
-        <SoccerField bgImage={`${process.env.REACT_APP_URL}/assets/image/bg_field.jpg`}>
-          <TeamContainer>
-            <PositionArea>
-              {
-                PlayerResult.myPlayer.map((home,index) => (
-                  home.spPosition === 0 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${home.imageId}.png`}
-                        team="home"
-                        name={home.name}
-                        originalName={home.originalName}
-                        positionName={home.positionDescription}
-                        positionId="GK"
-                        spPosition={home.spPosition}
-                        spGrade={home.spGrade}
-                        status={home.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.myPlayer.map((home,index) => (
-                  home.spPosition >= 1 && home.spPosition <= 8 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${home.imageId}.png`}
-                        team="home"
-                        name={home.name}
-                        originalName={home.originalName}
-                        positionName={home.positionDescription}
-                        positionId="D"
-                        spPosition={home.spPosition}
-                        spGrade={home.spGrade}
-                        status={home.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.myPlayer.map((home,index) => (
-                  home.spPosition >= 9 && home.spPosition <= 11 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${home.imageId}.png`}
-                        team="home"
-                        name={home.name}
-                        originalName={home.originalName}
-                        positionId="M"
-                        positionName={home.positionDescription}
-                        spPosition={home.spPosition}
-                        spGrade={home.spGrade}
-                        status={home.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.myPlayer.map((home,index) => (
-                  home.spPosition >= 12 && home.spPosition <= 16 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${home.imageId}.png`}
-                        team="home"
-                        name={home.name}
-                        originalName={home.originalName}
-                        positionName={home.positionDescription}
-                        positionId="M"
-                        spPosition={home.spPosition}
-                        spGrade={home.spGrade}
-                        status={home.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.myPlayer.map((home,index) => (
-                  home.spPosition >= 17 && home.spPosition <= 19 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${home.imageId}.png`}
-                        team="home"
-                        name={home.name}
-                        originalName={home.originalName}
-                        positionName={home.positionDescription}
-                        positionId="M"
-                        spPosition={home.spPosition}
-                        spGrade={home.spGrade}
-                        status={home.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.myPlayer.map((home,index) => (
-                  home.spPosition >= 20 && home.spPosition <= 22 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${home.imageId}.png`}
-                        team="home"
-                        name={home.name}
-                        originalName={home.originalName}
-                        positionName={home.positionDescription}
-                        positionId="S"
-                        spPosition={home.spPosition}
-                        spGrade={home.spGrade}
-                        status={home.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.myPlayer.map((home,index) => (
-                  home.spPosition >= 23 && home.spPosition <= 27 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${home.imageId}.png`}
-                        team="home"
-                        name={home.name}
-                        originalName={home.originalName}
-                        positionName={home.positionDescription}
-                        positionId="S"
-                        spPosition={home.spPosition}
-                        spGrade={home.spGrade}
-                        status={home.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-          </TeamContainer>
-
-          {/* away */}
-          <TeamContainer>
-            <PositionArea>
-              {
-                PlayerResult.enemyPlayer.map((away,index) => (
-                  away.spPosition >= 23 && away.spPosition <= 27 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${away.imageId}.png`}
-                        team="away"
-                        name={away.name}
-                        originalName={away.originalName}
-                        positionName={away.positionDescription}
-                        positionId="S"
-                        spPosition={away.spPosition}
-                        spGrade={away.spGrade}
-                        status={away.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.enemyPlayer.map((away,index) => (
-                  away.spPosition >= 20 && away.spPosition <= 22 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${away.imageId}.png`}
-                        team="away"
-                        name={away.name}
-                        originalName={away.originalName}
-                        positionName={away.positionDescription}
-                        spPosition={away.spPosition}
-                        positionId="S"
-                        spGrade={away.spGrade}
-                        status={away.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.enemyPlayer.map((away,index) => (
-                  away.spPosition >= 17 && away.spPosition <= 19 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${away.imageId}.png`}
-                        team="away"
-                        name={away.name}
-                        originalName={away.originalName}
-                        positionName={away.positionDescription}
-                        positionId="M"
-                        spPosition={away.spPosition}
-                        spGrade={away.spGrade}
-                        status={away.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.enemyPlayer.map((away,index) => (
-                  away.spPosition >= 12 && away.spPosition <= 16 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${away.imageId}.png`}
-                        team="away"
-                        name={away.name}
-                        originalName={away.originalName}
-                        positionName={away.positionDescription}
-                        spPosition={away.spPosition}
-                        positionId="M"
-                        spGrade={away.spGrade}
-                        status={away.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.enemyPlayer.map((away,index) => (
-                  away.spPosition >= 9 && away.spPosition <= 11 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${away.imageId}.png`}
-                        team="away"
-                        name={away.name}
-                        originalName={away.originalName}
-                        positionName={away.positionDescription}
-                        positionId="M"
-                        spPosition={away.spPosition}
-                        spGrade={away.spGrade}
-                        status={away.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.enemyPlayer.map((away,index) => (
-                  away.spPosition >= 1 && away.spPosition <= 8 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${away.imageId}.png`}
-                        team="away"
-                        name={away.name}
-                        originalName={away.originalName}
-                        positionName={away.positionDescription}
-                        positionId="D"
-                        spPosition={away.spPosition}
-                        spGrade={away.spGrade}
-                        status={away.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-            <PositionArea>
-              {
-                PlayerResult.enemyPlayer.map((away,index) => (
-                  away.spPosition === 0 ? (
-                      <Player 
-                        key={index}
-                        bgImage={`${process.env.REACT_APP_IMG_API_URL}${away.imageId}.png`}
-                        team="away"
-                        name={away.name}
-                        originalName={away.originalName}
-                        positionName={away.positionDescription}
-                        positionId="GK"
-                        spPosition={away.spPosition}
-                        spGrade={away.spGrade}
-                        status={away.status}
-                      />
-                  ) : null
-                ))
-              }
-            </PositionArea>
-          </TeamContainer>
-        </SoccerField>
-      </Container>
-    }
+    <PlayerList wide="left: 10px">
+      <Title>선수 명단</Title>
+      {
+        loading ? <Loading /> :
+        PlayerResult.myPlayer.map((player,index) => (
+          <PlayerListItem 
+            key={index} 
+            order={player.spPosition}
+          >
+            {
+              <>
+                <Position 
+                  bgColor={
+                    player.spPosition === 0 ? '#e9a216' : 
+                    player.spPosition > 0 && player.spPosition < 9 ? '#1476ff' :
+                    player.spPosition > 8 && player.spPosition < 20 ? '#03cd7a' :
+                    player.spPosition > 19 && player.spPosition < 28 ? '#f6425f' : '#c2c5ca'
+                  }
+                >{player.positionDescription}</Position>
+                <Name>{player.originalName}</Name>
+                <Price>{player.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} BP</Price>
+              </>
+            }
+          </PlayerListItem>
+        ))
+      }
+    </PlayerList>
+    <Stadium 
+      PlayerResult={PlayerResult}
+      ClubPrice={ClubPrice}
+      error={error}
+      loading={loading}
+    />
+    <PlayerList wide="right: 10px">
+      <Title>선수 명단</Title>
+      {
+        loading ? <Loading /> :
+        PlayerResult.enemyPlayer.map((player,index) => (
+          <PlayerListItem 
+            key={index}
+            order={player.spPosition}
+          >
+            {
+              <>
+                <Position 
+                  bgColor={
+                    player.spPosition === 0 ? '#e9a216' : 
+                    player.spPosition > 0 && player.spPosition < 9 ? '#1476ff' :
+                    player.spPosition > 8 && player.spPosition < 20 ? '#03cd7a' :
+                    player.spPosition > 19 && player.spPosition < 28 ? '#f6425f' : '#c2c5ca'
+                  }
+                >{player.positionDescription}</Position>
+                <Name>{player.originalName}</Name>
+                <Price>{player.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} BP</Price>
+              </>
+            }
+          </PlayerListItem>
+        ))
+      }
+    </PlayerList>
   </>
 )
 
