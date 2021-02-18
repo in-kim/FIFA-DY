2ce7449fbd2dfced5b568d0d26ac9490be9c1f51
in-kim <ehddls960617@gmail.com>
2021-02-18 16:32:06 +0900

    에드핏 추가

---
+++ src/Components/Advertisement.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+const Advertisement = (type,position) => (
+  <>
+    <ins className="kakao_ad_area" style={{display:'none'}} 
+      data-ad-unit    = "DAN-9Zqffsl36wMM59d3" 
+      data-ad-width   = "320" 
+      data-ad-height  = "100">
+    </ins> 
+    <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
+
+    <ins className="kakao_ad_area" style={{display:'none'}} 
+      data-ad-unit    = "DAN-52jaChpTsEM64lRc" 
+      data-ad-width   = "160" 
+      data-ad-height  = "600"></ins> 
+      <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
+  </>
+)
+export default Advertisement;
