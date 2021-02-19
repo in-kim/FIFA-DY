94f7343454ebaab72861bbf2c56e2debab545922
in-kim <ehddls960617@gmail.com>
2021-02-19 16:09:33 +0900

    base url 수정 및 유저정보 api 추가

--- src/api.js
+++ src/api.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:"https://fifa-record-api.link/api/record",
+  baseURL:"https://fifa-record-api.link/api",
 })
 
 export const matchList = {
-  list: (term,offset,limit) => api.get(`/match/${term}`,{
+  list: (term,offset,limit) => api.get(`/record/match/${term}`,{
     params:{
       offset:offset,
       limit:limit
     }
   }),
-  userInfo: (id) => api.get(`/user-info/${id}`),
+  userInfo: (id) => api.get(`/record/user-info/${id}`),
   detail: (nickname,id) => 
-  api.get(`/match-info/${nickname}/${id}`),
-  clubPrice: (nickname,id) => api.get(`/match-club-price?nickName=${nickname}&matchId=${id}`)
+  api.get(`/record/match-info/${nickname}/${id}`),
+  clubPrice: (nickname,id) => api.get(`/record/match-club-price?nickName=${nickname}&matchId=${id}`),
+  userResult: (id) => api.get(`/user/${id}`)
 }
