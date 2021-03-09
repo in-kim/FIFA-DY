e77927a59a5adcb7e210de5a9606839ad8983eba
in-kim <ehddls960617@gmail.com>
2021-03-09 18:38:01 +0900

    게시판 페이지 추가

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Home from "Routes/Home";
 import Detail from "Routes/Detail";
+import Board from "Routes/Board";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
@@ -10,6 +11,7 @@ export default () => (
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/detail/:name/:id/" component={Detail} />
+        <Route path="/board/:id" exact component={Board} />
         <Redirect from="*" to="/"/>
       </Switch>
     </>
