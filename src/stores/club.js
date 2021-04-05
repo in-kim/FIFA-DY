f064cd335cf217406c5cd8370b5a6e258dae27b6
in-kim <ehddls960617@naver.com>
2021-04-05 18:02:50 +0900

    구단정보 관련 스토어 생성

--- src/stores/club.js
+++ src/stores/club.js
@@ -1,18 +1,47 @@
-import { observable, makeObservable, action } from "mobx";
+import { observable, makeObservable, action, toJS } from "mobx";
 import { matchListApi } from 'api';
 
 export default class clubStore{
-  @observable clubData=null;
+  @observable clubSimpleData=null;
+  @observable clubDetailData=null;
+  @observable loading=false;
+  @observable error='';
 
   constructor(root) {
     makeObservable(this)
     this.root = root;
   }
 
-  // 닉네임 & 정보보기 클릭 시 호출
-  @action loadUserClubData = async (accessId) => {
-    const { data } = await matchListApi.userResult(accessId);
+  @action loadUserClubSimpleData = async () => {
+    const {search:{myAccessId}} = this.root;
+    this.loading = true;
+    try{
+      const {data} = await matchListApi.userInfo(myAccessId);
+      this.clubSimpleData = data;
+    }catch{
+      this.error = 'loadUserClubSimpleData error'
+    }finally{
+      this.loading = false;
+    }
+  }
 
-    this.clubData=data
+  // 닉네임 & 정보보기 클릭 시 호출
+  @action loadUserClubDetailData = async (accessId) => {
+    try {
+      // 닉네임 & 정보보기 클릭 시 호출
+      const {data} = await matchListApi.userResult(accessId);
+      this.clubDetailData = data;
+
+      console.log(toJS(this.clubDetailData));
+    } catch{
+      this.error='loadUserClubDetailData error'
+    } finally{
+      this.loading = false;
+    }
+  }
+
+  // 닉네임 & 정보보기 클릭 시 호출되는 팝업 닫기
+  @action clearUserClubDetailData = () => {
+    this.clubDetailData = null;
   }
 }
