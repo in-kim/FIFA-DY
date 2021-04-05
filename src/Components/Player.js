b28aeb5d2a3a3b7d2cdd3057f8e22b08b5ab3c95
in-kim <ehddls960617@naver.com>
2021-04-05 17:59:34 +0900

    1. 상하 값 중복되는 조건 삭제 2. 부모 컴포넌트(Stadium.js)에서 세부참조 해서 prop 해주는 부분 삭제 하고 해당 컴포넌트에서 데이터 세부참조

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -2,6 +2,126 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+const Player = ({bgImage, item, positionId}) => {
+  const {
+    spPosition, positionDescription:positionName,
+    name, originalName,
+    spGrade,
+    status
+  } = item;
+  console.log(item)
+  return(
+    <Container
+      top={
+          spPosition === 0 ? 'top: 45%' :
+          spPosition === 1 ? 'top: 45%' :
+          spPosition === 2 ? 'bottom: 4%' :
+          spPosition === 3 ? 'bottom: 10%' :
+          spPosition === 4 ? 'bottom: 30%' :
+          spPosition === 5 ? 'top: 45%' :
+          spPosition === 6 ? 'top: 30%' :
+          spPosition === 7 ? 'top: 10%' :
+          spPosition === 8 ? 'top: 4%' :
+          spPosition === 9 ? 'bottom:30%' :
+          spPosition === 10 ? 'bottom:45%' :
+          spPosition === 11 ? 'top:30%' :
+          spPosition === 12 ? 'top:10%' :
+          spPosition === 13 ? 'top:30%' :
+          spPosition === 14 ? 'top:45%' :
+          spPosition === 15 ? 'bottom:30%' :
+          spPosition === 16 ? 'bottom:10%' :
+          spPosition === 17 ? 'top:30%' :
+          spPosition === 18 ? 'top:45%' :
+          spPosition === 19 ? 'bottom:30%' :
+          spPosition === 20 ? 'top:30%' :
+          spPosition === 21 ? 'top:45%' :
+          spPosition === 22 ? 'bottom:30%' :
+          spPosition === 23 ? 'top:10%' :
+          spPosition === 24 ? 'top:30%' :
+          spPosition === 25 ? 'top:45%' :
+          spPosition === 26 ? 'bottom:30%' :
+          spPosition === 27 ? 'bottom:10%' : ''
+      }
+    >
+      <Image bgImage={bgImage}>
+        <Grade 
+          bgColor={
+            spGrade === 1 ? "#45494f" : 
+            spGrade > 4 && spGrade < 8? "#c2c5ca" : 
+            spGrade > 7 ? "#e8c337" : "#c37653"
+          }
+        >+ {spGrade}</Grade>
+      </Image>
+      <Position bgColor={
+          positionId === "GK" ? '#e9a216' : 
+          positionId === "D" ? '#1476ff' :
+          positionId === "M" ? '#03cd7a' : '#f6425f'
+      }>
+        {positionName}
+      </Position>
+      <Name>{name}</Name>
+  
+        {
+          status && (
+            <InfoPopup>
+              <InfoContainer>
+                <InfoTitle>{originalName}</InfoTitle>
+              </InfoContainer>
+              <InfoContainer>
+                <InfoItem>평점</InfoItem>
+                <InfoItem>⭐️{status.spRating}</InfoItem>
+              </InfoContainer>
+              <InfoContainer>
+                <InfoItem>어시스트</InfoItem>
+                <InfoItem>{status.block}</InfoItem>
+              </InfoContainer>
+              <InfoContainer>
+                <InfoItem>수비성공</InfoItem>
+                <InfoItem>{status.block}</InfoItem>
+              </InfoContainer>
+              <InfoContainer>
+                <InfoItem>드리블성공</InfoItem>
+                <InfoItem>{status.dribble}</InfoItem>
+              </InfoContainer>
+              <InfoContainer>
+                <InfoItem>유효슈팅</InfoItem>
+                <InfoItem>{status.effectiveShoot}</InfoItem>
+              </InfoContainer>
+              <InfoContainer>
+                <InfoItem>골</InfoItem>
+                <InfoItem>{status.goal}</InfoItem>
+              </InfoContainer>
+              <InfoContainer>
+                <InfoItem>슈팅횟수</InfoItem>
+                <InfoItem>{status.shoot}</InfoItem>
+              </InfoContainer>
+              <InfoContainer>
+                <InfoItem>패스시도</InfoItem>
+                <InfoItem>{status.passTry}</InfoItem>
+              </InfoContainer>
+              <InfoContainer>
+                <InfoItem>패스성공</InfoItem>
+                <InfoItem>{status.passSuccess}</InfoItem>
+              </InfoContainer>
+              <InfoContainer>
+                <InfoItem>태클성공</InfoItem>
+                <InfoItem>{status.tackle}</InfoItem>
+              </InfoContainer>
+            </InfoPopup>
+        )
+      }
+    </Container>
+  )
+}
+
+Player.propTypes = {
+  bgImage:PropTypes.string,
+  item:PropTypes.object,
+  positionId:PropTypes.string,
+}
+
+export default Player;
+
 const InfoPopup = styled.div`
   display:none;
   position:absolute;
@@ -94,163 +214,3 @@ const Position = styled.span`
   border-radius:3px 0 0 3px;
   text-align:center;
 `;
-
-const Player = ({
-  bgImage,
-  team, 
-  name,
-  originalName,
-  positionName,
-  positionId,
-  spPosition,
-  spGrade,
-  status,
-  handlePlayerInfoShow,
-}) => (
-  <Container
-    onMouseOver={handlePlayerInfoShow}
-    top={
-      team === 'home' ?
-        spPosition === 0 ? 'top: 45%' :
-        spPosition === 1 ? 'top: 45%' :
-        spPosition === 2 ? 'bottom: 4%' :
-        spPosition === 3 ? 'bottom: 10%' :
-        spPosition === 4 ? 'bottom: 30%' :
-        spPosition === 5 ? 'top: 45%' :
-        spPosition === 6 ? 'top: 30%' :
-        spPosition === 7 ? 'top: 10%' :
-        spPosition === 8 ? 'top: 4%' :
-        spPosition === 9 ? 'bottom:30%' :
-        spPosition === 10 ? 'bottom:45%' :
-        spPosition === 11 ? 'top:30%' :
-        spPosition === 12 ? 'top:10%' :
-        spPosition === 13 ? 'top:30%' :
-        spPosition === 14 ? 'top:45%' :
-        spPosition === 15 ? 'bottom:30%' :
-        spPosition === 16 ? 'bottom:10%' :
-        spPosition === 17 ? 'top:30%' :
-        spPosition === 18 ? 'top:45%' :
-        spPosition === 19 ? 'bottom:30%' :
-        spPosition === 20 ? 'top:30%' :
-        spPosition === 21 ? 'top:45%' :
-        spPosition === 22 ? 'bottom:30%' :
-        spPosition === 23 ? 'top:10%' :
-        spPosition === 24 ? 'top:30%' :
-        spPosition === 25 ? 'top:45%' :
-        spPosition === 26 ? 'bottom:30%' :
-        spPosition === 27 ? 'bottom:10%' : ''
-      : 
-        spPosition === 0 ? 'top: 45%' :
-        spPosition === 1 ? 'top: 45%' :
-        spPosition === 2 ? 'top: 4%' :
-        spPosition === 3 ? 'top: 10%' :
-        spPosition === 4 ? 'top: 30%' :
-        spPosition === 5 ? 'top: 45%' :
-        spPosition === 6 ? 'bottom: 30%' :
-        spPosition === 7 ? 'bottom: 10%' :
-        spPosition === 8 ? 'bottom: 4%' :
-        spPosition === 9 ? 'top:30%' :
-        spPosition === 10 ? 'top:45%' :
-        spPosition === 11 ? 'bottom:30%' :
-        spPosition === 12 ? 'top:10%' :
-        spPosition === 13 ? 'top:30%' :
-        spPosition === 14 ? 'top:45%' :
-        spPosition === 15 ? 'bottom:30%' :
-        spPosition === 16 ? 'bottom:10%' :
-        spPosition === 17 ? 'top:30%' :
-        spPosition === 18 ? 'top:45%' :
-        spPosition === 19 ? 'bottom:30%' :
-        spPosition === 20 ? 'top:30%' :
-        spPosition === 21 ? 'top:45%' :
-        spPosition === 22 ? 'bottom:30%' :
-        spPosition === 23 ? 'top:10%' :
-        spPosition === 24 ? 'top:30%' :
-        spPosition === 25 ? 'top:45%' :
-        spPosition === 26 ? 'bottom:30%' :
-        spPosition === 27 ? 'bottom:10%' : ''
-    }
-  >
-    <Image bgImage={bgImage}>
-      <Grade 
-        bgColor={
-          spGrade === 1 ? "#45494f" : 
-          spGrade > 4 && spGrade < 8? "#c2c5ca" : 
-          spGrade > 7 ? "#e8c337" : "#c37653"
-        }
-      >+ {spGrade}</Grade>
-    </Image>
-    <Position bgColor={
-        positionId === "GK" ? '#e9a216' : 
-        positionId === "D" ? '#1476ff' :
-        positionId === "M" ? '#03cd7a' : '#f6425f'
-    }>
-      {positionName}
-    </Position>
-    <Name>{name}</Name>
-
-      {
-        status && (
-          <InfoPopup>
-            <InfoContainer>
-              <InfoTitle>{originalName}</InfoTitle>
-            </InfoContainer>
-            <InfoContainer>
-              <InfoItem>평점</InfoItem>
-              <InfoItem>⭐️{status.spRating}</InfoItem>
-            </InfoContainer>
-            <InfoContainer>
-              <InfoItem>어시스트</InfoItem>
-              <InfoItem>{status.block}</InfoItem>
-            </InfoContainer>
-            <InfoContainer>
-              <InfoItem>수비성공</InfoItem>
-              <InfoItem>{status.block}</InfoItem>
-            </InfoContainer>
-            <InfoContainer>
-              <InfoItem>드리블성공</InfoItem>
-              <InfoItem>{status.dribble}</InfoItem>
-            </InfoContainer>
-            <InfoContainer>
-              <InfoItem>유효슈팅</InfoItem>
-              <InfoItem>{status.effectiveShoot}</InfoItem>
-            </InfoContainer>
-            <InfoContainer>
-              <InfoItem>골</InfoItem>
-              <InfoItem>{status.goal}</InfoItem>
-            </InfoContainer>
-            <InfoContainer>
-              <InfoItem>슈팅횟수</InfoItem>
-              <InfoItem>{status.shoot}</InfoItem>
-            </InfoContainer>
-            <InfoContainer>
-              <InfoItem>패스시도</InfoItem>
-              <InfoItem>{status.passTry}</InfoItem>
-            </InfoContainer>
-            <InfoContainer>
-              <InfoItem>패스성공</InfoItem>
-              <InfoItem>{status.passSuccess}</InfoItem>
-            </InfoContainer>
-            <InfoContainer>
-              <InfoItem>태클성공</InfoItem>
-              <InfoItem>{status.tackle}</InfoItem>
-            </InfoContainer>
-          </InfoPopup>
-      )
-    }
-  </Container>
-)
-
-
-Player.propTypes = {
-  bgImage:PropTypes.string,
-  team:PropTypes.string,
-  name:PropTypes.string,
-  originalName:PropTypes.string,
-  positionId:PropTypes.string,
-  positionName:PropTypes.string,
-  spPosition:PropTypes.number.isRequired,
-  spGrade:PropTypes.number,
-  status:PropTypes.object,
-}
-
-export default Player;
