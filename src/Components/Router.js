fac8316285ba2cadab0c0c20e60ee340cabf1346
in-kim <ehddls960617@naver.com>
2021-04-18 21:44:52 +0900

    import board -> import List 변경

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Home from "Routes/Home";
 import Detail from "Routes/Detail";
-import Board from "Routes/Board";
+import List from "Routes/Board/list";
 import BoardDetail from 'Routes/Board/view'
 import BoardEdit from 'Routes/Board/edit'
 
@@ -13,7 +13,7 @@ export default () => (
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/detail/:name/:id/" component={Detail} />
-        <Route path="/board/:page/list" exact component={Board} />
+        <Route path="/board/:page/list" exact component={List} />
         <Route path="/board/:page/detail/:contentId" exact component={BoardDetail} />
         <Route path="/board/:page/write" exact component={BoardEdit} />
         <Route path="/board/:page/edit/:contentId" exact component={BoardEdit} />
