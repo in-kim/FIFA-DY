523b8eaa6b3ae9eb868397cea00f4d5dcf285279
in-kim <ehddls960617@naver.com>
2021-04-18 21:50:08 +0900

    게시판 관련 api 추기ㅏ

--- src/api.js
+++ src/api.js
@@ -17,3 +17,18 @@ export const matchListApi = {
   clubPrice: (nickname,id) => api.get(`/record/match-club-price?nickName=${nickname}&matchId=${id}`),
   userResult: (id) => api.get(`/user/${id}`)
 }
+
+export const BoardApi = {
+  list: (boardId) => api.get(`/board/${boardId}/posts`),
+  detail: (postId) => api.get(`/board/posts/${postId}`),
+  edit:(boardId,creator,password,title,content) => 
+    api.post(`/board/${boardId}/post`,{
+      creator: creator,
+      postPassword: password,
+      title: title,
+      text: content,
+    }
+  ),
+  modify:(postId) => api.put(`/api/board/post/${postId}`),
+  delete:(postId) => api.delete(`api/board/posts/${postId}`),
+}
