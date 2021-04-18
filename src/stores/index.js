404b6ee07d00b794a7a65521cbe6e061758df7b5
in-kim <ehddls960617@naver.com>
2021-04-18 21:51:41 +0900

    board store 추가

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,10 +1,11 @@
 import SearchStore from "./search";
 import ClubStore from "./club";
-
+import BoardStore from "./board";
 class RootStore{
   constructor(){
     this.search = new SearchStore(this);
     this.clubData = new ClubStore(this);
+    this.board = new BoardStore(this);
   }
 }
 
