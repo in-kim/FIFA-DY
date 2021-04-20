0c23f7669914d5621985effeea25de796b2fb9f5
in-kim <ehddls960617@naver.com>
2021-04-20 21:29:14 +0900

    1. 게시판 수정,등록 url 변경 2. list 주소 변경

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Home from "Routes/Home";
 import Detail from "Routes/Detail";
-import List from "Routes/Board/list";
+import BoardList from "Routes/Board/List";
 import BoardDetail from 'Routes/Board/view'
 import BoardEdit from 'Routes/Board/edit'
 
@@ -13,10 +13,10 @@ export default () => (
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/detail/:name/:id/" component={Detail} />
-        <Route path="/board/:page/list" exact component={List} />
+        <Route path="/board/:page/list" exact component={BoardList} />
         <Route path="/board/:page/detail/:contentId" exact component={BoardDetail} />
-        <Route path="/board/:page/write" exact component={BoardEdit} />
-        <Route path="/board/:page/edit/:contentId" exact component={BoardEdit} />
+        <Route path="/board/:page/:type" exact component={BoardEdit} />
+        <Route path="/board/:page/:type/:contentId/:password" exact component={BoardEdit} />
         <Redirect from="*" to="/"/>
       </Switch>
     </>
