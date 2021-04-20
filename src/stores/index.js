d48a71db44d98d827359e32f03b1db4c139276f5
in-kim <ehddls960617@naver.com>
2021-04-20 21:33:25 +0900

    board store 제거

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,11 +1,9 @@
 import SearchStore from "./search";
 import ClubStore from "./club";
-import BoardStore from "./board";
 class RootStore{
   constructor(){
     this.search = new SearchStore(this);
     this.clubData = new ClubStore(this);
-    this.board = new BoardStore(this);
   }
 }
 
