8d3e0274f1c91c030f493bf09572532390cf578c
in-kim <ehddls960617@naver.com>
2022-04-01 12:49:40 +0900

    fix. https to http

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:"https://fifa-record-api.link/api",
+  baseURL:"http://fifa-record-api.link/api",
 })
 
 export const matchListApi = {
